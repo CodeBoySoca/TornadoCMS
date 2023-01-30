e8a200f546d760303b786ab39a3b0143e408ef46
KidMongoose <mongoose@kids-mbp.lan>
2023-01-30 11:43:56 -0800

    Added h1 heading to dashboard, project and server templates along with CSS and get method for server.handler.py

--- handlers/server_handler.py
+++ handlers/server_handler.py
@@ -3,3 +3,6 @@ import tornado.web
 class ServerHandler(tornado.web.RequestHandler):
     def post(self):
         pass
+
+    def get(self):
+        self.render('server.html', page_title='Add Server')
